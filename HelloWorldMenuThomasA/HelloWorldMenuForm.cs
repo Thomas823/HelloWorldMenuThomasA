8584c8bb54b41f376e5b2b9a6f5e18bd8be4c557
Thomas823 <35612259+Thomas823@users.noreply.github.com>
2018-02-16 12:35:49 -0500

    Initial commit

---
+++ HelloWorldMenuThomasA/HelloWorldMenuForm.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by: Thomas Aubin
+ * Created on: 07-Feb-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #4 - Hello World with Menu
+ * This program displays Hello World but with an exit menu
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldMenuThomasA
+{
+    public partial class frmHelloWorldMenu : Form
+    {
+        public frmHelloWorldMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Close the program
+            this.Close();
+        }
+    }
+}
